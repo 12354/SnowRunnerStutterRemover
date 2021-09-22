b9f40d8232b881399907c095df5caeac27accdc8
Marcel Kiesel <marci66@gmail.com>
2021-09-22 15:21:38 +0200

    fixed save path issue

--- SnowRunnerStutterStarter/Program.cs
+++ SnowRunnerStutterStarter/Program.cs
@@ -100,6 +100,7 @@ namespace SnowRunnerStutterRemover
                     var savedPath = LoadSnowRunnerPath();
                     if (savedPath != null)
                     {
+                        targetExe = savedPath;
                         return;
                     }
 
